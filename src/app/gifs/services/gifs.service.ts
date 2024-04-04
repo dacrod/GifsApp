7d983754d24086cbace7c881cc73da5f48ee3dca
Daniel Cuarental Rodriguez <dcuarental@hiberus.com>
2024-04-04 10:01:14 +0200

    Lazy loading added

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,9 @@ export class GifsService {
     private apiKey:       string = '9UYwoWRxZYHZgKsMda37l8dsq13JB32Y';
     private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
 
-    constructor( private http: HttpClient ) { }
+    constructor( private http: HttpClient ) {
+        this.loadLocalStorage();
+     }
 
     get tagsHistory() {
         return [...this._tagsHistory];
@@ -26,6 +28,25 @@ export class GifsService {
 
         this._tagsHistory.unshift( tag );
         this._tagsHistory = this.tagsHistory.splice(0,10);
+        this.saveLocalStorage();
+    }
+
+    private saveLocalStorage():void {
+        localStorage.setItem( 'history', JSON.stringify(this._tagsHistory) )
+    }
+
+    private loadLocalStorage():void {
+        if( localStorage.getItem('history') ) {
+            this._tagsHistory = JSON.parse( localStorage.getItem( 'history' )! );
+        } else {
+            return;
+        }
+
+        
+
+        if( !(this._tagsHistory.length > 0) ) return;
+
+        this.searchTag(this._tagsHistory[0]);
     }
 
     searchTag( tag: string ):void {
